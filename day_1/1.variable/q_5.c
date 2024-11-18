24f75340bb1185c8e62359bc12890d4c80264669
Anto Bivin <antobivin@gmail.com>
2024-11-18 15:46:01 +0530

    Update q_5.c

--- day_1/1.variable/q_5.c
+++ day_1/1.variable/q_5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     int x1, y1, x2, y2;
@@ -10,8 +9,20 @@ int main() {
     printf("Enter coordinates of second point (x2, y2): ");
     scanf("%d %d", &x2, &y2);
 
-    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    // Calculate the square root manually using a simple iterative method (Newton's method)
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    float sum_of_squares = (dx * dx) + (dy * dy);
+
+    // Square root calculation (Newton's method)
+    float approx = sum_of_squares / 2.0;
+    float better_approx = (approx + (sum_of_squares / approx)) / 2.0;
+    while (approx - better_approx > 0.00001 || better_approx - approx > 0.00001) {
+        approx = better_approx;
+        better_approx = (approx + (sum_of_squares / approx)) / 2.0;
+    }
+    distance = better_approx;
+
     printf("Distance = %.1f\n", distance);
     return 0;
 }
-        
